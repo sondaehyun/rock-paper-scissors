1b55993a66ed4a9e7bce0181dc80d5a13357f71b
sondaehyun <165316572+sondaehyun@users.noreply.github.com>
2024-05-04 13:01:54 -0400

    Initial commit
    rok-paper-scissors game from fCC tutorial

---
+++ app.js
@@ -0,0 +1,54 @@
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('user-choice')
+const resultDisplay = document.getElementById('result')
+const possibleChoices = document.querySelectorAll('button')
+let userChoice
+let computerChoice
+let result
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+    userChoice = e.target.id
+    userChoiceDisplay.innerHTML = userChoice
+    generateComputerChoice()
+    getResult()
+}))
+
+function generateComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3) + 1 // or you can use possibleChoices.length
+    
+    if (randomNumber === 1) {
+        computerChoice = 'Rock'
+    }
+    if (randomNumber === 2) {
+        computerChoice = 'Scissors'
+    }
+    if (randomNumber === 3) {
+        computerChoice = 'Paper'
+    }
+    computerChoiceDisplay.innerHTML = computerChoice
+}
+
+function getResult() {
+    if (computerChoice === userChoice) {
+        result = "It's a draw!"
+    }
+    if (computerChoice === 'Rock' && userChoice === "Paper") {
+        result = "You win!"
+    }
+    if (computerChoice === 'Rock' && userChoice === "Scissors") {
+        result = "You lose!"
+    }
+    if (computerChoice === 'Paper' && userChoice === "Scissors") {
+        result = "You win!"
+    }
+    if (computerChoice === 'Paper' && userChoice === "Rock") {
+        result = "You lose!"
+    }
+    if (computerChoice === 'Scissors' && userChoice === "Rock") {
+        result = "You win!"
+    }
+    if (computerChoice === 'Scissors' && userChoice === "Paper") {
+        result = "You lose!"
+    }
+    resultDisplay.innerHTML = result
+}
